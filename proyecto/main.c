1e754a965ef00d723582f5396fe6bd17000e4aba
joser3047 <joser3047@gmail.com>
2016-08-08 10:59:44 -0600

    crear disco

--- proyecto/main.c
+++ proyecto/main.c
@@ -3,31 +3,130 @@
 #include <string.h>
 
 
-char comando[1000];
+char comando[];
 
 void main()
 {
     int ctokens=1;
-    int tamano=0;
     system("clear");
     scanf(" %[^\n]s", comando);
-    char *token = strtok(comando, " ");
-    char *tokens = strtok(NULL, " ");
+    char *token;
+    char *tokens;
+    char *size="";    char *unit="";    char *name="";    char *path="";
+    char tamano[100]="";    char unidad[2]="";     char nombre[100]="";   char dir[100]="";
+    int contador = 0;
+    token = strtok(comando, " ");
     if (strcasecmp(token, "mkdisk") == 0) {
-    while(tokens != NULL){
-    char *comand = strtok(tokens, "::");
-    if(strcasecmp(comand, "-size") == 0){
-    comand = strtok(NULL, "::");
-    printf("%s", comand);
+    tokens = comando;
+    tokens = strtok(NULL, " ");
+    if(tokens != NULL){
+        size = tokens; contador++;
+    } tokens = strtok(NULL, " ");
+    if(tokens != NULL){
+        unit = tokens; contador++;
+    } tokens = strtok(NULL, " ");
+    if(tokens != NULL){
+        name = tokens; contador++;
+    } tokens = strtok(NULL, " ");
+    if(tokens != NULL){
+        path = tokens; contador++;
+    } int cont = 0;
+    while(cont < contador){
+    if(cont == 0 && strcasecmp(size, "") != 0){
+    tokens = strtok(size, "::");
+    if(strcasecmp(tokens, "-size") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(tamano, tokens);}
+    else if(strcasecmp(tokens, "+unit") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(unidad, tokens);}
+    else if(strcasecmp(tokens, "-path") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(dir, tokens);}
+    else if(strcasecmp(tokens, "-name") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(nombre, tokens);}
+    else{
+    tokens = strtok(NULL, "::");
+    printf("Error en el comando: %s", tokens);
     }
-    if(strcasecmp(comand, "+unit") == 0){
-    comand = strtok(NULL, "::");
-    printf("%s", comand);
     }
-    if(strcasecmp(comand, "-path") == 0){
-    comand = strtok(NULL, "::");
-    printf("%s", comand);
+    else if(cont == 1 && strcasecmp(unit, "") != 0){
+    tokens = strtok(unit, "::");
+    if(strcasecmp(tokens, "-size") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(tamano, tokens);}
+    else if(strcasecmp(tokens, "+unit") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(unidad, tokens);}
+    else if(strcasecmp(tokens, "-path") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(dir, tokens);}
+    else if(strcasecmp(tokens, "-name") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(nombre, tokens);}
+    else{
+    tokens = strtok(NULL, "::");
+    printf("Error en el comando: %s", tokens);
     }
     }
+    else if(cont == 2 && strcasecmp(name, "") != 0){
+    tokens = strtok(name, "::");
+    if(strcasecmp(tokens, "-size") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(tamano, tokens);}
+    else if(strcasecmp(tokens, "+unit") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(unidad, tokens);}
+    else if(strcasecmp(tokens, "-path") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(dir, tokens);}
+    else if(strcasecmp(tokens, "-name") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(nombre, tokens);}
+    else{
+    tokens = strtok(NULL, "::");
+    printf("Error en el comando: %s", tokens);
+    }
+    }
+    else if(cont == 3 && strcasecmp(path, "") != 0){
+    tokens = strtok(path, "::");
+    if(strcasecmp(tokens, "-size") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(tamano, tokens);}
+    else if(strcasecmp(tokens, "+unit") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(unidad, tokens);}
+    else if(strcasecmp(tokens, "-path") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(dir, tokens);}
+    else if(strcasecmp(tokens, "-name") == 0){
+    tokens = strtok(NULL, "::");
+    strcpy(nombre, tokens);}
+    else{
+    tokens = strtok(NULL, "::");
+    printf("Error en el comando: %s", tokens);
+    }
+    } cont++;
+    }
+    printf("El tamaño es: %s\n", tamano);
+    printf("El nombre es: %s\n", nombre);
+    printf("La Direccion es: %s\n", dir);
+    printf("La unidad es: %s\n", unidad);
+    if((strcasecmp(tamano,"")!=0)&&(strcasecmp(dir,"")!=0)&&(strcasecmp(nombre,"")!=0)){
+    if(dir[0] == '/'){
+        char com[100];
+        strcpy (com,  "mkdir -p '");
+        strncat(com, dir, strlen(path)-1);
+        strcat(com, "'");
+        system(com);
+    }
+    }
+    if(strcasecmp(unidad,"k")==0){
+                contador = 1;
+    }else{
+                contador = 1024;
+    }
+
     }
 }
